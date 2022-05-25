b15e717fee074368dd7c5342c6d23d9e3612c311
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-05-25 22:23:11 +0300

    Done Task4

---
+++ Seminar1/Task4.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая принимает на вход 
+# координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+# Пример:
+# A (3,6); B (2,1) -> 5,09
+# A (7,-5); B (1,-1) -> 7,21
+from math import sqrt
+
+def Get_distance(x_first,y_first,x_second,y_second):
+    x_distance = x_second - x_first
+    y_distance = y_second - y_first
+
+    return sqrt((x_distance**2) + (y_distance**2))
+
+x_first = float(input('Input x of point A: '))
+y_first = float(input('Input y of point A: '))
+x_second = float(input('Input x of point B: '))
+y_second = float(input('Input y of point B: '))
+
+distance = Get_distance(x_first,y_first,x_second,y_second)
+print(f'Расстояние между точками равняется {round(distance,2)}')
